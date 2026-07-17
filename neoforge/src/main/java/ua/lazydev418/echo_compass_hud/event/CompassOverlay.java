9945ab4127743ff2597fb4d836280f3b86af9375
Vadym <61838855+lazydev418@users.noreply.github.com>
2026-07-17 17:33:55 +0300

    Clean up imports in CompassOverlay.java

--- neoforge/src/main/java/ua/lazydev418/echo_compass_hud/event/CompassOverlay.java
+++ neoforge/src/main/java/ua/lazydev418/echo_compass_hud/event/CompassOverlay.java
@@ -1,8 +1,8 @@
 package ua.lazydev418.echo_compass_hud.event;
 
 import ua.lazydev418.echo_compass_hud.gui.CompassHud;
-import ua.lazydev418.echo_compass_hud.util.IPlayerTabOverlay; //
-import ua.lazydev418.echo_compass_hud.util.Constants; //
+import ua.lazydev418.echo_compass_hud.util.IPlayerTabOverlay;
+import ua.lazydev418.echo_compass_hud.util.Constants;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.neoforge.client.event.RenderGuiLayerEvent;
@@ -11,7 +11,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 
 @EventBusSubscriber(
-        modid = Constants.MOD_ID, //
+        modid = Constants.MOD_ID,
         value = Dist.CLIENT
 )
 public class CompassOverlay {

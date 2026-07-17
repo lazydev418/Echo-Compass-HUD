4af6bcb1c2f8ad297226551be1c9b8c4c8c1fbed
Vadym <61838855+lazydev418@users.noreply.github.com>
2026-07-17 15:41:45 +0300

    Setup NeoForge

--- neoforge/src/main/java/ua/lazydev418/echo_compass_hud/EchoCompassHUD.java
+++ neoforge/src/main/java/ua/lazydev418/echo_compass_hud/EchoCompassHUD.java
@@ -1,12 +1,13 @@
 package ua.lazydev418.echo_compass_hud;
 
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import ua.lazydev418.echo_compass_hud.config.ClientConfig;
 import ua.lazydev418.echo_compass_hud.util.Constants;
 
 @Mod(Constants.MOD_ID)
 public class EchoCompassHUD {
-    public EchoCompassHUD() {
+    public EchoCompassHUD(IEventBus eventBus) {
         ClientConfig.load();
     }
 }

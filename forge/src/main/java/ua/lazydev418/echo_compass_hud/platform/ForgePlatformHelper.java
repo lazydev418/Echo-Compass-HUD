29bf1d9939ff45607ba2ab3076b54d5ca60ce017
Vadym <61838855+lazydev418@users.noreply.github.com>
2026-07-17 14:22:39 +0300

    .

---
+++ forge/src/main/java/ua/lazydev418/echo_compass_hud/platform/ForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package ua.lazydev418.echo_compass_hud.platform;
+
+import ua.lazydev418.echo_compass_hud.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}

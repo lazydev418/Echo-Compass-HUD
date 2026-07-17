1f4ef39d074edfe4a3133fb8ef8ccd1d075386e7
Vadym <61838855+lazydev418@users.noreply.github.com>
2026-07-17 14:22:49 +0300

    .

---
+++ neoforge/src/main/java/ua/lazydev418/echo_compass_hud/EchoCompassHUD.java
@@ -0,0 +1,12 @@
+package ua.lazydev418.echo_compass_hud;
+
+import net.neoforged.fml.common.Mod;
+import ua.lazydev418.echo_compass_hud.config.ClientConfig;
+import ua.lazydev418.echo_compass_hud.util.Constants;
+
+@Mod(Constants.MOD_ID)
+public class EchoCompassHUD {
+    public EchoCompassHUD() {
+        ClientConfig.load();
+    }
+}
---
+++ neoforge/src/main/java/ua/lazydev418/echo_compass_hud/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package ua.lazydev418.echo_compass_hud.platform;
+
+import com.example.examplemod.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}

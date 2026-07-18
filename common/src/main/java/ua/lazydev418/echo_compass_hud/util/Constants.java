a54dff6448b5c50daeeed742c432c87e8bdcf282
Vadym <61838855+lazydev418@users.noreply.github.com>
2026-07-18 16:32:08 +0300

    Update settings

--- common/src/main/java/ua/lazydev418/echo_compass_hud/util/Constants.java
+++ common/src/main/java/ua/lazydev418/echo_compass_hud/util/Constants.java
@@ -1,14 +1,11 @@
 package ua.lazydev418.echo_compass_hud.util;
 
 import net.minecraft.resources.ResourceLocation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Constants {
 
     public static final String MOD_ID = "echo_compass_hud";
     public static final String MOD_NAME = "Echo Compass HUD";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     public static final ResourceLocation COMPASS_BACKGROUND = ResourceLocation.fromNamespaceAndPath(MOD_ID, "textures/gui/compass_background.png");
 }

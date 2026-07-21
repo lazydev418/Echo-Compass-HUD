709a98a124ef2ebb7b83b7a3225f915905fd1d8f
Vadym <61838855+lazydev418@users.noreply.github.com>
2026-07-21 13:49:49 +0300

    Rename ResourceLocation

--- common/src/main/java/ua/lazydev418/echo_compass_hud/util/Constants.java
+++ common/src/main/java/ua/lazydev418/echo_compass_hud/util/Constants.java
@@ -1,11 +1,11 @@
 package ua.lazydev418.echo_compass_hud.util;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public class Constants {
 
     public static final String MOD_ID = "echo_compass_hud";
     public static final String MOD_NAME = "Echo Compass HUD";
 
-    public static final ResourceLocation COMPASS_BACKGROUND = ResourceLocation.fromNamespaceAndPath(MOD_ID, "textures/gui/compass_background.png");
+    public static final Identifier COMPASS_BACKGROUND = Identifier.fromNamespaceAndPath(MOD_ID, "textures/gui/compass_background.png");
 }
